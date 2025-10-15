612fd61a9cf93d342098065b4e69ff5351be0f5c
khakimovallamurod <allamurod0206@gmail.com>
2025-10-15 18:39:17 +0500

    change route

--- flask_app.py
+++ flask_app.py
@@ -19,52 +19,58 @@ def decode_response_text(resp_text: str) -> str:
     return decoded.strip()
 
 
-@app.route("/chat", methods=["POST"])
+@app.route("/ai/chat/getdata", methods=["POST"])
 def chat_with_samdu():
-    URL = config.get_url()
-    data = request.get_json()
+    if request.method == 'POST':
 
-    if not data or "message" not in data:
-        return jsonify({"error": "Iltimos, 'message' maydonini yuboring."}), 400
+        URL = config.get_url()
+        data = request.get_json()
 
-    user_message = data["message"]
+        if not data or "message" not in data:
+            return jsonify({"error": "Iltimos, 'message' maydonini yuboring."}), 400
 
-    payload = {
-        "blockId": "cmgroxc0l0009nqgqabp29veu",
-        "params": {
+        user_message = data["message"]
+
+        payload = {
+            "blockId": "cmgroxc0l0009nqgqabp29veu",
             "params": {
-                "publishedPageParams": ["samdu"]
+                "params": {
+                    "publishedPageParams": ["samdu"]
+                }
+            },
+            "stream": True,
+            "chatbotSessionId": "cmgrxwbgc015z5l0y9h8dixna",
+            "predictionId": "853ff38c-b601-497e-b503-176bbfa73be3",
+            "message": {
+                "content": user_message,
+                "parts": [
+                    {"type": "text", "text": user_message}
+                ],
+                "role": "user"
             }
-        },
-        "stream": True,
-        "chatbotSessionId": "cmgrxwbgc015z5l0y9h8dixna",
-        "predictionId": "853ff38c-b601-497e-b503-176bbfa73be3",
-        "message": {
-            "content": user_message,
-            "parts": [
-                {"type": "text", "text": user_message}
-            ],
-            "role": "user"
         }
-    }
 
-    headers = {"Content-Type": "application/json"}
-    
-    response = requests.post(URL, json=payload, headers=headers)
-    result_text = decode_response_text(resp_text = response.text)
-    if response.status_code != 200:
+        headers = {"Content-Type": "application/json"}
+        
+        response = requests.post(URL, json=payload, headers=headers)
+        result_text = decode_response_text(resp_text = response.text)
+        if response.status_code != 200:
+            return jsonify({
+                "status": "error",
+                "user_message": user_message,
+                "response": None
+            })
+
         return jsonify({
+            "status": "success",
+            "user_message": user_message,
+            "response": result_text
+        })
+    return jsonify({
             "status": "error",
             "user_message": user_message,
-            "response": None
+            "response": "Method not found!"
         })
-
-    return jsonify({
-        "status": "success",
-        "user_message": user_message,
-        "response": result_text
-    })
-
 
 if __name__ == "__main__":
     app.run(debug=True)
